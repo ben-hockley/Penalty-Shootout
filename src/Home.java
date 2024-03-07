f8d8b4287b2de1d1a23d7e3cb17132e80d79f897
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-07 22:10:14 +0000

    Made basic homescreen with title and button

--- src/Home.java
+++ src/Home.java
@@ -2,10 +2,34 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Home extends JFrame {
+    JPanel TitlePanel = new JPanel();
+    JPanel ButtonPanel = new JPanel();
+    JLabel Title = new JLabel("PENALTY SHOOTOUT");
+    JButton PlayButton = new JButton("Play");
+
     Home(){
+        this.setTitle("Penalty Shootout");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(1000,500);
+        this.setResizable(false);
         this.setLayout(new BorderLayout());
         this.setVisible(true);
+
+        Title.setFont(new Font("SansSerif Bold",Font.BOLD,80));
+        Title.setForeground(Color.white);
+
+
+        PlayButton.setSize(200,50);
+
+        TitlePanel.setPreferredSize(new Dimension(1000,300));
+        TitlePanel.setBackground(Color.blue);
+        TitlePanel.add(Title);
+
+        ButtonPanel.setPreferredSize(new Dimension(1000,200));
+        ButtonPanel.setBackground(Color.green);
+        ButtonPanel.add(PlayButton);
+
+        this.add(TitlePanel,BorderLayout.NORTH);
+        this.add(ButtonPanel,BorderLayout.SOUTH);
     }
 }
