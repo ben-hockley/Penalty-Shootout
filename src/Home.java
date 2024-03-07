71696b06812263a3522990b26082c3f71d0d7838
c23037255 <HockleyB@cardiff.ac.uk>
2024-03-07 21:35:50 +0000

    Made blank JFrame for homescreen

---
+++ src/Home.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Home extends JFrame {
+    Home(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1000,500);
+        this.setLayout(new BorderLayout());
+        this.setVisible(true);
+    }
+}
